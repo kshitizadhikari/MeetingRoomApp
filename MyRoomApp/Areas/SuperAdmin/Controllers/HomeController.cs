97d1629979a148f2b9ab5612c6c90a2464e43c5a
seaweed78 <adhikarikshitiz12@gmail.com>
2023-09-06 21:18:51 +0545

    AddEditUserRoleSuperAdmin
    q

--- MyRoomApp/Areas/SuperAdmin/Controllers/HomeController.cs
+++ MyRoomApp/Areas/SuperAdmin/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using RoomApp.DataAccess.DAL;
 using RoomApp.Models;
 using RoomApp.Utility.ViewModels;
@@ -18,7 +19,7 @@ namespace MyRoomApp.Areas.SuperAdmin.Controllers
 
         public HomeController(AppDbContext db, UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
         {
-           _db = db;
+            _db = db;
             this._userManager = userManager;
             this._roleManager = roleManager;
         }
@@ -28,7 +29,7 @@ namespace MyRoomApp.Areas.SuperAdmin.Controllers
             IEnumerable<ApplicationUser> users = _db.Users.ToList();
             List<UserRoleVM> userWithRoles = new List<UserRoleVM>();
 
-            foreach(var user in users)
+            foreach (var user in users)
             {
                 var role = await _userManager.GetRolesAsync(user);
                 string roleString = string.Join(", ", role);
@@ -44,5 +45,76 @@ namespace MyRoomApp.Areas.SuperAdmin.Controllers
             }
             return View(userWithRoles);
         }
+
+        public async Task<IActionResult> EditUser(string? id)
+        {
+            if (id == null)
+            {
+                TempData["error"] = "Invalid Id";
+                return RedirectToAction("Index");
+            }
+
+            var user = await _db.Users.FindAsync(id);
+            if (user == null)
+            {
+                TempData["error"] = "User doesn't exist.";
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Roles = await _db.Roles.ToListAsync();
+            var role = await _userManager.GetRolesAsync(user);
+            string roleString = string.Join(", ", role);
+
+            UserRoleVM userWithRole = new UserRoleVM
+            {
+                UserId = user.Id,
+                UserName = user.UserName,
+                Email = user.Email,
+                Role = roleString
+            };
+
+            return View(userWithRole);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> EditUserRole(UserRoleVM obj)
+        {
+            if (obj == null)
+            {
+                TempData["error"] = "Invalid object passed";
+                return RedirectToAction("Index");
+            }
+
+            var user = await _db.Users.FindAsync(obj.UserId);
+            if (user == null)
+            {
+                TempData["error"] = "No such user exists.";
+                return RedirectToAction("Index");
+            }
+
+            var currentRoles = await _userManager.GetRolesAsync(user);
+
+            foreach (var role in currentRoles)
+            {
+                var result = await _userManager.RemoveFromRoleAsync(user, role);
+                if (!result.Succeeded)
+                {
+                    TempData["error"] = "Couldn't remove the user's roles.";
+                    return RedirectToAction("Index");
+
+                }
+            }
+
+            string newRole = obj.Role.ToString();
+            var addToRoleResult = await _userManager.AddToRoleAsync(user, newRole);
+            if (!addToRoleResult.Succeeded)
+            {
+                TempData["error"] = "Couldn't add the user's roles.";
+                return RedirectToAction("Index");
+            }
+            return RedirectToAction("Index");
+        }
     }
+
 }
