c8ff6f5f5d0757fefbe489ff35c07460887ec9a4
seaweed78 <adhikarikshitiz12@gmail.com>
2023-09-11 14:16:39 +0545

    BasicUser-BookRoom, ToastrAdd

--- MyRoomApp/Areas/BasicUser/Controllers/HomeController.cs
+++ MyRoomApp/Areas/BasicUser/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using Microsoft.EntityFrameworkCore;
 using RoomApp.DataAccess.DAL;
 using RoomApp.Models;
 using RoomApp.Utility.ViewModels;
+using System.Security.Claims;
 
 namespace MyRoomApp.Areas.BasicUser.Controllers
 {
@@ -19,42 +20,109 @@ namespace MyRoomApp.Areas.BasicUser.Controllers
             _db = db;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var currentUser = HttpContext?.User?.FindFirstValue(ClaimTypes.NameIdentifier);
+            string UserId = currentUser?.ToString() ?? "DefaultUserId";
+            ViewBag.UserId = UserId;
+            List<RoomBookingVM> vmList = new List<RoomBookingVM>();
+            List<Booking> allBookings = _db.Bookings.ToList();
+            foreach (Booking booking in allBookings)
+            {
+                Room? room = await _db.Rooms.FindAsync(booking.RoomId);
+                var bookUser = await _db.Users.FindAsync(booking.UserId);
+                string? bookUserId = bookUser.Id;
+
+                vmList.Add(new RoomBookingVM
+                {
+                    UserId = bookUserId,
+                    Room = room,
+                    BookingId = booking.Id,
+                    BookingName = booking.Name,
+                    StartTime = booking.StartTime,
+                    EndTime = booking.EndTime,
+                });
+            }
+            return View(vmList);
         }
 
         public async Task<IActionResult> ViewRooms()
-        {
-            IEnumerable<Room> allRooms = await _db.Rooms.ToListAsync();
-            return View(allRooms);
-        }
-
-        public async Task<IActionResult> BookRoom(int? id)
-        {
-            if (id == null || id == 0)
             {
-                TempData["error"] = "Invalid Id.";
-                return RedirectToAction("Index");
+                IEnumerable<Room> allRooms = await _db.Rooms.ToListAsync();
+                return View(allRooms);
             }
 
-            Room? roomObj = await _db.Rooms.FindAsync(id);
-            if (roomObj == null)
+            public async Task<IActionResult> BookRoom(int? id)
             {
-                TempData["error"] = "Room Not Found.";
-                return RedirectToAction("Index");
+                if (id == null || id == 0)
+                {
+                    TempData["error"] = "Invalid Id.";
+                    return RedirectToAction("Index");
+                }
+
+                Room? roomObj = await _db.Rooms.FindAsync(id);
+                if (roomObj == null)
+                {
+                    TempData["error"] = "Room Not Found.";
+                    return RedirectToAction("Index");
+                }
+
+                RoomBookingVM roomBookObj = new RoomBookingVM
+                {
+                    Room = roomObj,
+                    BookingId = 0,
+                    BookingName = "",
+                    StartTime = null,
+                    EndTime = null
+                };
+                return View(roomBookObj);
             }
 
-            RoomBookingVM roomBookObj = new RoomBookingVM
+            [HttpPost]
+            [ValidateAntiForgeryToken]
+            public async Task<IActionResult> BookRoom(RoomBookingVM? roomBook)
             {
-                Room = roomObj,
-                BookingId = 0,
-                BookingName = "",
-                StartTime = null,
-                EndTime = null
-            };
+                if (roomBook == null)
+                {
+                    TempData["error"] = "Room Booking Unsuccessful.";
+                }
 
-            return View(roomBookObj);
+                var currentUserId = HttpContext?.User?.FindFirstValue(ClaimTypes.NameIdentifier);
+                string currentUserIdString = currentUserId?.ToString() ?? "DefaultUserId";
+                ApplicationUser? userObj = await _db.Users.FindAsync(currentUserIdString);
+
+                List<Booking> allBookings = _db.Bookings.ToList();
+                foreach (var booking in allBookings)
+                {
+                    if (booking.UserId == currentUserIdString)
+                    {
+                        if (roomBook.StartTime >= booking.StartTime && roomBook.StartTime < booking.EndTime)
+                        {
+                            TempData["error"] = "Starting Time Conflict. Choose a different starting time.";
+                            return RedirectToAction("BookRoom", new { id = roomBook.Room.Id });
+                        }
+                        else if (roomBook.EndTime > booking.StartTime && roomBook.EndTime <= booking.EndTime)
+                        {
+                            TempData["error"] = "End Time Conflict. Choose a different ending time.";
+                            return RedirectToAction("BookRoom", new { id = roomBook.Room.Id });
+                        }
+                    }
+                }
+
+                Booking? bookingObj = new Booking
+                {
+                    Name = roomBook.BookingName,
+                    RoomId = roomBook.Room?.Id ?? 0,
+                    StartTime = roomBook.StartTime,
+                    EndTime = roomBook.EndTime,
+                    UserId = currentUserIdString,
+                    User = userObj
+                };
+
+                _db.Bookings.Add(bookingObj);
+                await _db.SaveChangesAsync();
+                TempData["success"] = "Room Booked Successfully";
+                return RedirectToAction("Index");
+            }
         }
     }
-}
